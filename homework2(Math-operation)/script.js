fd82f16c695c4c38c91de2325078b1f5feb03ef7
KostyaDemchenko <kostya.doge@gmail.com>
2022-11-29 15:35:00 +0200

    homework2

---
+++ homework2(Math-operation)/script.js
@@ -0,0 +1,13 @@
+let firstNum = +prompt('Enter fisrt Num');
+
+let secondNum = +prompt('Enter second Num');
+
+const minusValue = firstNum - secondNum;
+const divideValue = firstNum / secondNum;
+const multiValue = firstNum * secondNum;
+const plusValue = firstNum + secondNum;
+
+alert(firstNum + ' + ' + secondNum + ' = ' + plusValue + '\n'
++ firstNum + ' - ' + secondNum + ' = ' + minusValue + '\n'
++ firstNum + ' / ' + secondNum + ' = ' + divideValue + '\n'
++ firstNum + ' * ' + secondNum + ' = ' + multiValue);
