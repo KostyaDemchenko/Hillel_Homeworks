39d36b457d58b9ddf1f9fbe02b126a8abbcfaf7a
KostyaDemchenko <kostya.doge@gmail.com>
2022-12-05 16:03:33 +0200

    homework4

---
+++ homework4(Calculator)/script.js
@@ -0,0 +1,23 @@
+const mathOperator = prompt('Which operation do you want to do?' + '\n' +
+'(add/sub/mult/div)').trim().toLowerCase();
+
+const firstNum = +prompt('Enter first num');
+const secondNum = +prompt('Enter second num');
+
+let solve;
+
+if ((mathOperator == 'add') || (mathOperator == '+')){
+    alert(`${firstNum} + ${secondNum} = ${firstNum + secondNum}`);
+}
+else if ((mathOperator == 'sub') || (mathOperator == '-')){
+    alert(`${firstNum} - ${secondNum} = ${firstNum - secondNum}`);
+}
+else if ((mathOperator == 'mult') || (mathOperator == '*')){
+    alert(`${firstNum} * ${secondNum} = ${firstNum * secondNum}`);
+}
+else if ((mathOperator == 'div') || (mathOperator == '/')){
+    alert(`${firstNum} / ${secondNum} = ${firstNum / secondNum}`);
+}
+else {
+    alert('Error!');
+}
