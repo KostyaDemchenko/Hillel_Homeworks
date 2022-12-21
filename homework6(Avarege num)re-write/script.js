23e41cfd14a7ce2740bf53208d8674c453f3b3f1
KostyaDemchenko <kostya.doge@gmail.com>
2022-12-21 12:04:33 +0200

    re write 6 hw

---
+++ homework6(Avarege num)re-write/script.js
@@ -0,0 +1,8 @@
+const firstNum = parseInt(prompt('Enter first num...'));
+const secondNum = parseInt(prompt('Enter second num...'));
+const thirdNum = parseInt(prompt('Enter third num...'));
+
+const sumNum = (firstNum + secondNum + secondNum);
+const avarageNum = (sumNum / 3);
+
+alert('Your avarege num is ' + avarageNum);
