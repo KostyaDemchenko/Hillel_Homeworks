5a26a0471a160147b23450c1afca033ffe642129
KostyaDemchenko <kostya.doge@gmail.com>
2023-01-31 02:40:16 +0200

    Fix-removed_unused_parameter

--- homework4(Calculator)/script.js
+++ homework4(Calculator)/script.js
@@ -1,25 +1,17 @@
 const mathOperator = prompt('Which operation do you want to do?' + '\n' +
-'(add/sub/mult/div)').trim().toLowerCase();
+    '(add/sub/mult/div)').trim().toLowerCase();
 
 const firstNum = +prompt('Enter first num');
 const secondNum = +prompt('Enter second num');
 
-let solve;
-
-if ((mathOperator == 'add') || (mathOperator == '+')){
+if ((mathOperator == 'add') || (mathOperator == '+')) {
     alert(`${firstNum} + ${secondNum} = ${firstNum + secondNum}`);
-}
-else if ((mathOperator == 'sub') || (mathOperator == '-')){
+} else if ((mathOperator == 'sub') || (mathOperator == '-')) {
     alert(`${firstNum} - ${secondNum} = ${firstNum - secondNum}`);
-}
-else if ((mathOperator == 'mult') || (mathOperator == '*')){
+} else if ((mathOperator == 'mult') || (mathOperator == '*')) {
     alert(`${firstNum} * ${secondNum} = ${firstNum * secondNum}`);
-}
-else if ((mathOperator == 'div') || (mathOperator == '/')){
+} else if ((mathOperator == 'div') || (mathOperator == '/')) {
     alert(`${firstNum} / ${secondNum} = ${firstNum / secondNum}`);
-}
-else {
+} else {
     alert('Error!');
 }
-
-//Ипользуем метиод .trim и .toLowerCase для того чтобы отсеять ненуждные ответы для понимания нашего скрипта
