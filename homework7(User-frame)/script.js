bad376c60b5441326bc2d9ff83a48d3485e5adf0
KostyaDemchenko <kostya.doge@gmail.com>
2023-02-01 18:51:43 +0200

    HW7-Another_fix

--- homework7(User-frame)/script.js
+++ homework7(User-frame)/script.js
@@ -1,46 +1,39 @@
 const userAge = prompt('Enter your age');
 const userCity = prompt('Enter your city', 'Kiev, Amsterdam, Madrid');
-const userActivity = prompt('Say your favorite sport ?', 'Football, Basketball, Golf');
+const userActivity = prompt('Enter your favorite sport', 'Football, Basketball, Golf');
 
-let alertSport = '';
-let alertCity = '';
+let resultMessage = '';
 
-// Age validation
 if (userAge === null || isNaN(userAge) || parseInt(userAge) <= 0) {
-    alert('You did not enter a valid age!');
+    resultMessage = 'You did not enter a valid age!';
+} else if (userCity === null || userCity.trim() === '') {
+    resultMessage = 'You did not enter a valid city!';
+} else if (userActivity === null || userActivity.trim() === '') {
+    resultMessage = 'You did not enter a valid activity!';
 } else {
-    // City validation
-    if (userCity === null || userCity.trim() === '') {
-        alert('You did not enter a valid city!');
-    } else {
-        // Activity validation
-        if (userActivity === null || userActivity.trim() === '') {
-            alert('You did not enter a valid activity!');
-        } else {
-
-            switch (userActivity.trim()) {
-                case 'Football':
-                    alertSport = 'Do you want to be a Messi?';
-                    break;
-                case 'Bbasketball':
-                    alertSport = 'Do you want to be Kobe Bryant?';
-                    break;
-                case 'Golf':
-                    alertSport = 'Do you want to be Bernhard Langer?';
-                    break;
-                default:
-                    alertSport = 'I wish you success in this sport';
-            }
-            switch (userCity.trim()) {
-                case 'Kiev':
-                case 'Amsterdam':
-                case 'Madrid':
-                    alertCity = 'Oh cool you live in the capital!';
-                    break;
-                default:
-                    alertCity = 'You live in a beautiful city!';
-            }
-            alert('You are ' + userAge + ' years old \n' + alertCity + '\n' + alertSport);
-        }
+    resultMessage = 'You are ' + userAge + ' years old\n';
+    switch (userCity.trim()) {
+        case 'Kiev':
+        case 'Amsterdam':
+        case 'Madrid':
+            resultMessage += 'Oh cool, you live in the capital!\n';
+            break;
+        default:
+            resultMessage += 'You live in a beautiful city!\n';
+    }
+    switch (userActivity.trim()) {
+        case 'Football':
+            resultMessage += 'Do you want to be Messi?';
+            break;
+        case 'Basketball':
+            resultMessage += 'Do you want to be Kobe Bryant?';
+            break;
+        case 'Golf':
+            resultMessage += 'Do you want to be Bernhard Langer?';
+            break;
+        default:
+            resultMessage += 'I wish you success in this sport';
     }
 }
+
+alert(resultMessage);
